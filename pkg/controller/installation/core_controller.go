7dada2bdd6807164660bb0afeabea24fb2665fa0
varalakshmi kumar <vara@tigera.io>
2022-05-25 10:16:54 -0700

    SAAS 2084 changes-Adding conditions for CRs from tigerastatus

--- pkg/controller/installation/core_controller.go
+++ pkg/controller/installation/core_controller.go
@@ -752,6 +752,7 @@ func (r *ReconcileInstallation) Reconcile(ctx context.Context, request reconcile
 
 	// Changes for updating installation status conditions
 	if request.Name == InstallationName && request.Namespace == "" {
+
 		ts := &operator.TigeraStatus{}
 		err := r.client.Get(ctx, types.NamespacedName{Name: InstallationName}, ts)
 		if err != nil {
